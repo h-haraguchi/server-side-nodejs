d08279deac932efbc6124dd9730ee8d33f426e46
h-haraguchi <6197504+h-haraguchi@users.noreply.github.com>
2019-07-11 00:29:18 +0900

    try passportjs

--- src/express.ts
+++ src/express.ts
@@ -1,14 +1,29 @@
 import express from 'express';
 import {logger} from './logger';
+import passport from 'passport';
+import {Strategy, VerifyFunction} from 'passport-local';
 
 const PORT = 3000;
 
-export function startWebServer() {
-	const app = express();
+const alwaysSuccessVerification: VerifyFunction = (username, password, done) => {
+	logger.application.info(`呼ばれてるよー${username}${password}`);
+	return done(null, username);
+};
+passport.use(new Strategy(alwaysSuccessVerification));
+passport.serializeUser(function(user, cb) { cb(null, 1); });
+passport.deserializeUser(function(id, cb) { cb(null, {}); });
 
-	app.use("/", express.static('public'));
+const app = express();
 
-	app.listen(PORT, () => logger.application.info(`Example app listening on port ${PORT}!`));
-}
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
-startWebServer();
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/", express.static('public'));
+app.get("/login", express.static('public/login'));
+app.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login', session: false}));
+
+app.listen(PORT, () => logger.application.info(`Example app listening on port ${PORT}!`));
